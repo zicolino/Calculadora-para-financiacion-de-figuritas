1049e6d752650cfe85e7527a665c2ccb664faacb
zicolino <167160363+zicolino@users.noreply.github.com>
2024-04-22 20:52:42 -0300

    sacando al pelado

--- JS/sript.js
+++ JS/sript.js
@@ -1,13 +1,13 @@
-let dinero = prompt(`Cuanto dinero tenés?`)
+let dinero = prompt(`Cuanta plata tenés?`)
 
-if ((dinero >= 0) && (dinero <= 20)){
-    alert("Te puedo ofrecer 2 caramelos")
+if ((dinero >= 0) && (dinero <= 235)){
+    alert("Te puedo ofrecer 2 caramelos.")
 }
 
-if ((dinero >= 21) && (dinero <= 49)){
+if ((dinero >= 236) && (dinero <= 500)){
     alert("Estas cerca, pero no puedo fiarte, vuelve mañana.")
 }
 
-if (dinero >= 50){
-    alert("Aquí tienes tus figuritas del mundial")
+if (dinero >= 501){
+    alert("Aquí tiene sus figuritas del mundial.")
 }
